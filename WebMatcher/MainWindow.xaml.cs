9d994da6adf2189af882315ac9b875458260c0dc
Mathieu GRENET <mathieu@lesbouys.net>
2015-03-21 21:29:42 +0100

    cleanup

--- WebMatcher/MainWindow.xaml.cs
+++ WebMatcher/MainWindow.xaml.cs
@@ -1,26 +1,15 @@
-﻿using Microsoft.Win32;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.IO;
-using System.Linq;
 using System.Net;
-using System.Reflection;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Windows.Threading;
-using System.Drawing;
 using System.Windows.Controls.Primitives;
 using System.Globalization;
 using System.Threading;
@@ -33,7 +22,7 @@ namespace WebMatcher
     /// </summary>
 
 
-   public partial class MainWindow : Window, INotifyPropertyChanged
+    public partial class MainWindow : Window, INotifyPropertyChanged
    {
        public event PropertyChangedEventHandler PropertyChanged;
 
